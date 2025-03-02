7d44b62e4f9e46e0db3452a3421dca1e7f7f6bf8
Amr Younis <amr.younis@stu.najah.edu>
2025-03-02 12:43:36 +0200

    create cool animation on scroll

---
+++ scroll animation/script.js
@@ -0,0 +1,15 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+checkBoxes()
+function checkBoxes() {
+  const triggerPoint = (window.innerHeight / 5) * 4
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top
+    console.log(window.innerHeight, boxTop, triggerPoint)
+
+    if (boxTop < triggerPoint) {
+      box.classList.add('show')
+    } else box.classList.remove('show')
+  })
+}
